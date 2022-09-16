318b53c98ef0df1aebe11832e861f960a5b5db2d
MakarovAndriy <makarov.andrii.10@gmail.com>
2022-09-16 09:27:22 +0300

    Labs

---
+++ Labs/Lab_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int H, R;
+            double V;
+            bool isHOk = false, isROk = false;
+
+            do
+            {
+                Console.Write("Введіть висоту циліндра. H = ");
+                isHOk = int.TryParse(Console.ReadLine(), out H);
+                if (!isHOk)
+                    Console.WriteLine("Помилка введення. Спробуйте ще раз.");
+            } while (!isHOk);
+
+            do
+            {
+                Console.Write("Введіть радіус основи циліндра. R = ");
+                isROk = int.TryParse(Console.ReadLine(), out R);
+                if (!isROk)
+                    Console.WriteLine("Помилка введення. Спробуйте ще раз.");
+            } while (!isROk);
+            V = Math.PI * R * R * H;
+            Console.WriteLine($"Об'єм циліндра. V = {V}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Labs/Lab_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nn, nk;
+            double result = 0;
+            bool isNnOk = false, isNkOk = false;
+
+            do
+            {
+                do
+                {
+                    Console.Write("Введіть перший індекс. nn = ");
+                    isNnOk = int.TryParse(Console.ReadLine(), out nn);
+                    if (!isNnOk)
+                        Console.WriteLine("Помилка введення. Спробуйте ще раз.");
+                } while (!isNnOk);
+
+                do
+                {
+                    Console.Write("Введіть другий індекс. nk = ");
+                    isNkOk = int.TryParse(Console.ReadLine(), out nk);
+                    if (!isNkOk)
+                        Console.WriteLine("Помилка введення. Спробуйте ще раз.");
+                } while (!isNkOk);
+                if (!(0 <= nn && nn <= nk))
+                    Console.WriteLine("Помилка введення. nn має бути більше нуля та менше nk.");
+            } while (!(0 <= nn && nn <= nk));
+            for (int k = nn; k <= nk; k++)
+                result += (Math.Pow(-1, k * k + 1) * k * k - 2) / (3 * k * k - 2 * k);
+            Console.WriteLine($"Сума з {nn} по {nk} елементів ряду дорівнює {result}");
+            Console.ReadKey();
+        }
+    }
+}
