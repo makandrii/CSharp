928baa78de7ed8fea6360b4f8e03f41c98a7c517
MakarovAndriy <makarov.andrii.10@gmail.com>
2022-10-05 17:50:08 +0300

    Labs

---
+++ Labs/Lab_4/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+namespace Lab_4
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***21 варіант***");
+            const int N = 10;
+            int[] example = new int[N];
+            Random random = new();
+            for (int i = 0; i < N; i++)
+                example[i] = random.Next(10);
+
+            Console.Write("Початковий масив: ");
+            Print(example);
+            Console.WriteLine();
+
+            Console.Write("1) ");
+            Print(ClearSamples(example, 2, '<'));
+            Console.WriteLine();
+
+            Console.Write("2) ");
+            Print(ClearSamples(example, 2, '>'));
+            Console.WriteLine();
+
+            Console.Write("3) ");
+            Print(ClearSamples(example, 2, '='));
+            Console.WriteLine();
+
+            Console.Write("4) ");
+            Print(ClearSamples(example, 3, '='));
+            Console.WriteLine();
+        }
+        public static int[] ClearSamples(int[] a, int countOfSamples, char operation)
+        {
+            var samples = CountOfSamples(a);
+            List<int> result = new(), willRemoved = new();
+            foreach (var pair in samples)
+            {
+                if (ChooseOperation(operation)(pair.count, countOfSamples))
+                {
+                    willRemoved.Add(pair.value);
+                }
+            }
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (willRemoved.Contains(a[i]))
+                    continue;
+                result.Add(a[i]);
+            }
+            return result.ToArray();
+        }
+        public static List<(int value, int count)> CountOfSamples(int[] a)
+        {
+            List<(int value, int count)> result = new();
+            for (int i = 0; i < a.Length; i++)
+            {
+                int counter = 1;
+                for (int j = 0; j < a.Length; j++)
+                {
+                    if (a[i] == a[j] && i != j)
+                        counter++;
+                }
+                result.Add((a[i], counter));
+            }
+            return result;
+        }
+        public static Action? ChooseOperation(char operation)
+        {
+            switch (operation)
+            {
+                case '=': return (int a, int b) => a == b;
+                case '>': return (int a, int b) => a > b;
+                case '<': return (int a, int b) => a < b;
+                default: return null;
+            }
+        }
+        public static void Print(in int[] matrix)
+        {
+            for (int i = 0; i < matrix.Length; i++)
+                Console.Write($"{matrix[i]}\t");
+        }
+        public delegate bool Action(int a, int b);
+    }
+}
---
+++ Labs/Lab_5/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Lab_5
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***21 варіант***");
+            const int rows = 4, cols = 10;
+            int[,] matrix = new int[rows, cols];
+            Random random = new();
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < cols; j++)
+                    matrix[i, j] = random.Next(10);
+
+            Print(matrix);
+            Console.WriteLine();
+
+            int k1, k2;
+            k1 = int.Parse(Console.ReadLine());
+            k2 = int.Parse(Console.ReadLine());
+
+            Swap(ref matrix, k1, k2, "row");
+            Print(matrix);
+            Console.WriteLine();
+
+            Swap(ref matrix, k1, k2, "col");
+            Print(matrix);
+            Console.ReadKey();
+        }
+        public static void Swap(ref int[,] matrix, int k1, int k2, string type)
+        {
+            switch (type)
+            {
+                case "row":
+                    for (int j = 0; j < matrix.Length / matrix.GetLength(k1); j++)
+                        (matrix[k1, j], matrix[k2, j]) = (matrix[k2, j], matrix[k1, j]);
+                    break;
+                case "col":
+                    for (int i = 0; i < matrix.GetLength(k1); i++)
+                        (matrix[i, k1], matrix[i, k2]) = (matrix[i, k2], matrix[i, k1]);
+                    break;
+            }
+        }
+        public static void Print(in int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = 0; j < matrix.Length / matrix.GetLength(0); j++)
+                    Console.Write($"{matrix[i, j]}{((j + 1) % (matrix.Length / matrix.GetLength(0)) == 0 ? '\n' : '\t')}");
+        }
+    }
+}
